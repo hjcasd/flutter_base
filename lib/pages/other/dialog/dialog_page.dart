e310579b0fdd4ed2a0b2a097c255d77026dc3b27
hjcasd <2959383735@qq.com>
2022-05-23 10:00:50 +0800

    更新

--- lib/pages/other/dialog/dialog_page.dart
+++ lib/pages/other/dialog/dialog_page.dart
@@ -208,7 +208,7 @@ class _MyDialogState extends State<MyDialog> {
         title: "温馨提示",
         content: "对非中国大陆籍会员暂不支持开通钱包，如您需进行积分消费，可开通消费密码。",
         cancelText: "稍后",
-        // confirmText: "开通消费免密",
+        confirmText: "开通消费免密",
         onConfirmCallback: () {
           GetHelper.showSnackBar("开通消费免密");
         },
